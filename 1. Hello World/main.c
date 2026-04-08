73282b5c6dcd861882848e21ca6a150efcb77e3f
Snigdho <snigdhotf@gmail.com>
2026-04-08 23:44:46 +0200

    first commit

---
+++ 1. Hello World/main.c
@@ -0,0 +1,13 @@
+// https://www.hackerrank.com/challenges/hello-world-c/problem
+
+#include <stdio.h>
+int main()
+{
+    char s[100];
+    scanf("%[^\n]%*c", s);
+
+    printf("Hello, World!\n");
+    printf("%s", s);
+
+    return 0;
+}
