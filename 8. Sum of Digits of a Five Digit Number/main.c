f06d2dd6d2a64002161bc941107c51d5fc16e2f9
Snigdho <snigdhotf@gmail.com>
2026-05-03 12:22:05 +0200

    added problem 8

---
+++ 8. Sum of Digits of a Five Digit Number/main.c
@@ -0,0 +1,17 @@
+// https://www.hackerrank.com/challenges/sum-of-digits-of-a-five-digit-number/problem
+
+#include <stdio.h>
+int main() {
+
+  int n;
+  scanf("%d", &n);
+
+  int sum = 0;
+  while (n > 0) {
+    sum += n % 10;
+    n /= 10;
+  }
+  printf("%d\n", sum);
+
+  return 0;
+}
