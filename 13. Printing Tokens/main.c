80676d7feea8550af7ca143d4a9afd3c3680296f
Snigdho <snigdhotf@gmail.com>
2026-05-08 23:54:32 +0200

    added problem 13

---
+++ 13. Printing Tokens/main.c
@@ -0,0 +1,22 @@
+// https://www.hackerrank.com/challenges/printing-tokens-/problem
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main() {
+
+  char *s;
+  s = malloc(1024 * sizeof(char));
+  scanf("%[^\n]", s);
+  s = realloc(s, strlen(s) + 1);
+
+  for (int i = 0; s[i] != '\0'; i++) {
+    if (s[i] == ' ')
+      s[i] = '\n';
+  }
+  printf("%s", s);
+  free(s);
+
+  return 0;
+}
